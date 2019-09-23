5dadc2f6d102cd6a883ca4bd5e10ef554d64fe0e
mathieu sevigny lavallee <mathieeu@outlook.com>
2019-09-23 16:26:14 -0400

    login

--- frontend/site/src/app/app-routing.module.ts
+++ frontend/site/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
