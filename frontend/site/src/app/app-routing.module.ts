4415185fa6a0ddf57ea9c60116b4510d50d9bed2
mathieu sevigny lavallee <mathieeu@outlook.com>
2019-09-23 18:21:27 -0400

    login form

--- frontend/site/src/app/app-routing.module.ts
+++ frontend/site/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  { path: '', component: AppComponent},
-  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent},
+  { path: '', component: LoginComponent },
 ];
 
 @NgModule({
--- frontend/site/src/app/app.module.ts
+++ frontend/site/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule, MatInputModule, MatButtonModule } from '@angular/material';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [
     
