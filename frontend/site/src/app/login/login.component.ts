4cd80659e52cc4e0b18f2a3c0be988f33400801c
mathieu sevigny lavallee <mathieeu@outlook.com>
2019-09-23 19:07:10 -0400

    login form

---
+++ frontend/site/src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
