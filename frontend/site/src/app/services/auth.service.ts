fb03c33351a204017cd4d8635f67ac405249295a
Charles Bourgeois <charlesbourgeois@live.ca>
2019-09-25 15:21:23 -0400

    trying to fix cors allow problem

--- frontend/site/src/app/services/auth.service.ts
+++ frontend/site/src/app/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from '../models';
 import { config } from '../../config';
+
 
 @Injectable({
   providedIn: 'root'
@@ -23,7 +24,12 @@ export class AuthService {
     }
 
     login(username: string, password: string) {
-        return this.http.post<any>(`${config.apiUrl}/api/login`, { username, password })
+        var data = "username="+username+"&password="+password+"&grant_type=password";
+        var reqHeader = new HttpHeaders({'Content-Type': 'application/x-www-urlencoded'});
+        return this.http.post(`http://127.0.0.1:8000/api/login`,data,{headers:reqHeader});
+        
+        
+        /*return this.http.post<any>(`http://127.0.0.1:8000/api/login`, { username, password })
             .pipe(map(user => {
                 if (user && user.token) {
                     localStorage.setItem('currentUser', JSON.stringify(user));
@@ -31,7 +37,7 @@ export class AuthService {
                 }
 
                 return user;
-            }));
+            }));*/
     }
 
     logout() {
