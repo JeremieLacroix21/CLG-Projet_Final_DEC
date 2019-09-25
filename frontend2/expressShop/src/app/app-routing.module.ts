fce32dfd51d3c357d5c6ff580e9fa0e488bcc284
mathieu sevigny lavallee <mathieeu@outlook.com>
2019-09-25 17:33:45 -0400

    ahah

--- frontend2/expressShop/src/app/app-routing.module.ts
+++ frontend2/expressShop/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeModule} from './home/home.module';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'home', loadChildren: () => import(`./home/home.module`).then(m => m.HomeModule)},
+  {path: '', redirectTo: 'login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
