fce72ffbd75f84a00fb0b47026e345ca70363e35
Charles Bourgeois <charlesbourgeois@live.ca>
2019-09-25 17:18:15 -0400

    fucntion

--- backend/BackLaravel/app/Http/Controllers/API/ProduitController.php
+++ backend/BackLaravel/app/Http/Controllers/API/ProduitController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Produit;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Validator;
 
 class ProduitController extends Controller
 {
