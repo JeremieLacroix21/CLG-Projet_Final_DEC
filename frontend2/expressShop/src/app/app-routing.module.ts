609f968fb123238fceed84c8c76f6bea7d11c6ad
mathieu sevigny lavallee <mathieeu@outlook.com>
2019-09-25 19:08:13 -0400

    frontend 2 bby

--- frontend2/expressShop/src/app/app-routing.module.ts
+++ frontend2/expressShop/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { HomeModule} from './home/home.module';
-
+import { AuthGuard } from './guard';
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'home', loadChildren: () => import(`./home/home.module`).then(m => m.HomeModule)},
-  {path: '', redirectTo: 'login', pathMatch: 'full'}
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'home',
+    loadChildren: () => import(`./home/home.module`).then(m => m.HomeModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  }
 ];
 
 @NgModule({
