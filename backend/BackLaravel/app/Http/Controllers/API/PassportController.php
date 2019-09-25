734b526f0d3590dad519b24c1fd27ee442b94bea
Charles Bourgeois <charlesbourgeois@live.ca>
2019-09-24 22:37:41 -0400

    backend

--- backend/BackLaravel/app/Http/Controllers/API/PassportController.php
+++ backend/BackLaravel/app/Http/Controllers/API/PassportController.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Validator;
+
 class PassportController extends Controller
 {
    public $successStatus = 200;
@@ -13,14 +15,15 @@ class PassportController extends Controller
     *
     * @return \Illuminate\Http\Response
     */
-   public function login(){
-       if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+   public function login(Request $request){
+       $credentials = $request->only('name', 'password');
+       if(Auth::attempt($credentials)){
            $user = Auth::user();
            $success['token'] =  $user->createToken('MyApp')->accessToken;
            return response()->json(['success' => $success], $this->successStatus);
        }
        else{
-           return response()->json(['error'=>'Unauthorised'], 401);
+           return response()->json(['error'=> $credentials], 401);
        }
    }
    /**
@@ -32,9 +35,15 @@ class PassportController extends Controller
    {
        $validator = Validator::make($request->all(), [
            'name' => 'required',
-           'email' => 'required|email',
            'password' => 'required',
            'c_password' => 'required|same:password',
+           'nom' => 'required',
+           'prenom' => 'required',
+           'TypeUser' => 'required',
+           'adresse' => 'required',
+           'admin' => 'required',
+           'confirme' => 'required',
+           'dateinscription' => 'required',
        ]);
        if ($validator->fails()) {
            return response()->json(['error'=>$validator->errors()], 401);            
@@ -44,6 +53,17 @@ class PassportController extends Controller
        $user = User::create($input);
        $success['token'] =  $user->createToken('MyApp')->accessToken;
        $success['name'] =  $user->name;
+       DB::table('user')->insert(array(
+        'nomutilisateur' =>  $input['name'],
+        'motdepasse' => $input['password'],
+        'nom' => $input['nom'],
+        'prenom' => $input['prenom'],
+        'TypeUser' => $input['TypeUser'],
+        'adresse' => $input['adresse'],
+        'admin' => $input['admin'],
+        'confirme' => $input['confirme'],
+        'dateinscription' => $input['dateinscription']
+       ));
        return response()->json(['success'=>$success], $this->successStatus);
    }
    /**
