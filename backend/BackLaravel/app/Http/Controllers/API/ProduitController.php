b6f29f4308f6ebfb9655b092824d721a85981a58
Charles Bourgeois <charlesbourgeois@live.ca>
2019-09-25 17:03:49 -0400

    get all produit

---
+++ backend/BackLaravel/app/Http/Controllers/API/ProduitController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Http\Controllers\API;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Produit;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Validator;
+
+class ProduitController extends Controller
+{
+   public function ShowProduct()
+   {
+        $produit = DB::table('produits')->get();
+        $data = [];
+        foreach($produit as $produits) { }
+        {
+            $data[$produits->idproduits] = [$produits->nom_produit, $produits->type,$produits->prix, 
+            $produits->idfourniseur,$produits->enstock];
+        }
+        return json_encode($data);
+    }
+}
