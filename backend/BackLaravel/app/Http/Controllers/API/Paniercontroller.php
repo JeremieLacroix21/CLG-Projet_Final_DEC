07bf5f4e234157cbda5e4978940d7acb164969cd
JeremieLacroix21 <Jrmie@DESKTOP-JL90G99>
2019-10-02 16:46:27 -0400

    Create Paniercontroller.php

---
+++ backend/BackLaravel/app/Http/Controllers/API/Paniercontroller.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Http\Controllers\API;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Mail;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Mail\EmailSender;
+
+class PassportController extends Controller
+{
+   public $successStatus = 200;
+   /**
+    * login api
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function GetPanier(Request $request){
+   
+
+    }
+
+
+   
+        
+
+
+}
