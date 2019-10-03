1caeea16db6c592e8d41da8f9941410276da0881
GabrielF-C <45073321+GabrielF-C@users.noreply.github.com>
2019-10-02 20:03:58 -0400

    .

--- frontend2/expressShop/src/app/home/browse-products/browse-products.component.ts
+++ frontend2/expressShop/src/app/home/browse-products/browse-products.component.ts
@@ -6,7 +6,6 @@ import { Product } from '../../models/product';
 import { Router, ActivatedRoute } from "@angular/router";
 import { Supplier } from 'src/app/models/supplier';
 import { first } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-browse-products',
@@ -15,19 +14,72 @@ import { Observable } from 'rxjs';
 })
 export class BrowseProductsComponent implements OnInit
 {
-  loadedProducts : any[];
-  constructor(private productservice: ProductService) {
+  max = 9999;
+  min = 0;
+  // Contains all the products got from the api
+  // TODO: Load the products in loadProducts() instead of using the sample
+  private loadedProducts : Product[];
+  // Contains all the owners(suppliers) that own products in the array above
+  // Example: supplierMap[products[0].idfournisseur].nomutilisateur
+  //private supplierMap: { [key:number]:Supplier } = {};
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private productservice: ProductService) {
+      this.GetProducts();
     }
 
   ngOnInit() {
-    this.GetProducts();
   }
-  
+  // Gets a list of products from the API.
+  // TODO: Include parameters for searches and filters
+
+  /* private loadProducts() {
+    // TODO: Call the API to get the products
+
+    // Get a list of all the products' owners and map them to their id
+    for(let product of this.loadedProducts) {
+      // Check if the supplier is already loaded
+      if (typeof this.supplierMap[product.idfournisseur] === 'undefined') {
+        // TODO: Call the API using `product.idfournisseur` to get the supplier
+        //this.supplierMap[product.idfournisseur] = LOADED_SUPPLIER;
+      }
+    }
+  } */
+
+  /* private onClickFavoriteBtn(event) {
+    let btnId = event.currentTarget.id;
+    let btn = document.getElementById(btnId);
+    let splitBtnId = btnId.split('-');
+    let productId = splitBtnId[splitBtnId.length-1];
+
+    // Switch the displayed icon
+    for(var i = 0; i < btn.children.length; ++i) {
+      btn.children[i].classList.toggle('mdc-icon-button__icon--on');
+    }
+
+    // Toggle the favorite attribute
+    btn.attributes['favorite'].value = (btn.attributes['favorite'].value === 'false' ? 'true' : 'false');
+
+    let newFavoriteValue = false;
+    for(var i = 0; i < this.loadedProducts.length; ++i) {
+      if (this.loadedProducts[i].idproduits == productId) {
+        newFavoriteValue = (btn.attributes['favorite'].value === 'true');
+        break;
+      }
+    }
+  } */
+
   GetProducts(){
-    this.productservice.GetProduct().subscribe(
+    this.productservice
+    .GetProduct()
+    .pipe(first())
+    .subscribe(
         data => {
-          this.loadedProducts = data;
+          this.loadedProducts = data['loadedProducts'];
           console.log(data);
-        });
+        }
+    );
   }
+
 }
