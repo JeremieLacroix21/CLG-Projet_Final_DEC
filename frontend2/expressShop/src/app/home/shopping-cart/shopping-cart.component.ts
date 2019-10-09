d342057c06dc4681d3a4652c9206d3fc8cd9c013
JeremieLacroix21 <Jrmie@DESKTOP-JL90G99>
2019-10-09 13:41:33 -0400

    commit

--- frontend2/expressShop/src/app/home/shopping-cart/shopping-cart.component.ts
+++ frontend2/expressShop/src/app/home/shopping-cart/shopping-cart.component.ts
@@ -27,32 +27,9 @@ export class ShoppingCartComponent implements OnInit {
 
   displayedColumns: string[] = ['image', 'id', 'nom', 'prix', 'quantité', 'sous-total'];
   NOMPAGE = "Votre Panier";
-  TABelement : productPanier[] = [
-    {
-      id: 1,
-      nom: "allo",
-      prix: 900,
-      photo:"allo.jpg",
-      qty: 10
-    },
-    {
-      id: 2,
-      nom: "allo2",
-      prix: 40,
-      photo:"allo.jpg",
-      qty: 10
-    },
-    {
-      id: 3,
-      nom: "allo3",
-      prix: 10,
-      photo:"allo.jpg",
-      qty: 10
-    },
+  TABelement : productPanier[];
+  dataSource : MatTableDataSource<productPanier>;
   
-  ];
-  
-  dataSource = new MatTableDataSource<productPanier>(this.TABelement);
   total : number;
 
 
@@ -63,28 +40,26 @@ export class ShoppingCartComponent implements OnInit {
   filteredProducts: productPanier[];
 
   constructor(private productService: ProductService, private loader: LoaderService) { 
-      this.Total();
+      
         this.subscription = this.productService.GetpanierFromId(+localStorage.getItem('currentUser')).
           subscribe( products => { this.filteredProducts = this.products = products
               setTimeout(() => {  this.loader.hide();
               });
             });
-          
+
+            this.TABelement = this.filteredProducts;
+            this.dataSource = new MatTableDataSource<productPanier>(this.TABelement);
+            this.Total();
   }
   ngOnInit() {
-
     this.dataSource.paginator = this.paginator;
   }
-
-
   increment(column)
   {
     this.TABelement[column].qty += 1;    
     this.Total();
       
   }
-
-
   decrement(column)
   {
     if(this.TABelement[column].qty - 1 == 0)
@@ -97,25 +72,15 @@ export class ShoppingCartComponent implements OnInit {
     }
     this.Total();
   }
-  set(i)  
-  {
-    
-  } 
-
-
   delete(column)
   {
     delete this.TABelement[column];
     document.getElementById("tr" + column).remove();
   }
-
-
   SousTotal(i) : string
   {
     return (this.TABelement[i].prix * this.TABelement[i].qty).toString();
   }
-
-
   Total()
   {
     this.total = 0;
@@ -124,8 +89,6 @@ export class ShoppingCartComponent implements OnInit {
         this.total += this.TABelement[i].prix * this.TABelement[i].qty;
     }
   }
-
-
   deleteProductFromCart(iduser:number,idproduit:number)
   {
       this.productService.DeleteProductFromCart(iduser,idproduit);
