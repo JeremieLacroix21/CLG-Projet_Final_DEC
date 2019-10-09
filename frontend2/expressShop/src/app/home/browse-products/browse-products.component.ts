d127210353252a721ddd0a2bb96db8da2b677f36
mathieu sevigny lavallee <mathieeu@outlook.com>
2019-10-09 15:32:25 -0400

    filtre

--- frontend2/expressShop/src/app/home/browse-products/browse-products.component.ts
+++ frontend2/expressShop/src/app/home/browse-products/browse-products.component.ts
@@ -11,8 +11,11 @@ import { LoaderService } from 'src/app/services/loader.service';
 })
 export class BrowseProductsComponent implements OnInit, OnDestroy
 {
+  max=0;
+  min=0;
   subscription: Subscription;
   products: Product[];
+  Tproducts: Product[];
   filteredProducts: Product[];
 
   constructor(private productService: ProductService, private loader: LoaderService) {
@@ -36,10 +39,13 @@ export class BrowseProductsComponent implements OnInit, OnDestroy
     this.productService.AddProductToCart(iduser,idproduct,1);
   }
 
-  private Filter(chaine:string, idf:string){
+  private Filter(chaine:string, idf:string, pmin:number, pmax:number){
     this.filteredProducts = this.products;
-
-    console.log(this.filteredProducts)
+    this.Tproducts = null;
+    var newArray = new Array();
+    console.log(chaine);
+    console.log(pmin);
+    console.log(pmax);
 
     this.filteredProducts = (chaine) ?
       this.filteredProducts.filter(p => p.nom.toLowerCase().includes(chaine.toLowerCase())):
@@ -48,6 +54,28 @@ export class BrowseProductsComponent implements OnInit, OnDestroy
     this.filteredProducts = (idf) ?
       this.filteredProducts.filter(p => p.nomFournisseur.toLowerCase().includes(idf.toLowerCase())):
         this.filteredProducts;
+
+    if(pmax){
+      var j= 0
+      for (var i = 0; i < this.filteredProducts.length; i++) {
+        if (this.filteredProducts[i].prix <= pmax) {
+          newArray[j] = this.filteredProducts[i];
+          j++;
+        }
+      }
+      this.filteredProducts = newArray;
+    } 
+    newArray = new Array();
+    if(pmin){
+      var j= 0
+      for (var i = 0; i < this.filteredProducts.length; i++) {
+        if (this.filteredProducts[i].prix >= pmin) {
+          newArray[j] = this.filteredProducts[i];
+          j++;
+        }
+      }
+      this.filteredProducts = newArray;
+    }
   }
 
 
