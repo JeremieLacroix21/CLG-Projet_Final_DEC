5c4fbff7e79640a7dae11863c48517e5a50e83c8
mathieu sevigny lavallee <mathieeu@outlook.com>
2019-10-22 20:16:39 -0400

    add product html

--- frontend2/expressShop/src/app/home/add-product/add-product.component.ts
+++ frontend2/expressShop/src/app/home/add-product/add-product.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+
+export interface tag {
+  name: string;
+}
 
 @Component({
   selector: 'app-add-product',
@@ -8,14 +14,47 @@ import { FormGroup, Validators, FormControl } from '@angular/forms';
 })
 export class AddProductComponent implements OnInit {
 
+  
+
   productForm = new FormGroup({
     nom: new FormControl('', Validators.required),
-    
+    description: new FormControl('', Validators.required),
+    prix: new FormControl('', Validators.required),
+    quantite: new FormControl('', Validators.required),
   })
 
   constructor() { }
 
   ngOnInit() {
   }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  tags: tag[] = [
+  ];
 
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.tags.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(Tag: tag): void {
+    const index = this.tags.indexOf(Tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
 }
