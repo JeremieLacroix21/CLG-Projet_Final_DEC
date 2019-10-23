1e668ddb9ee16e3c0ae08e9077cfcdf22e092e59
mathieu sevigny lavallee <matieeu@outlook.com>
2019-10-23 15:02:44 -0400

    ajout produit

--- frontend2/expressShop/src/app/home/add-product/add-product.component.ts
+++ frontend2/expressShop/src/app/home/add-product/add-product.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { v4 as uuid } from 'uuid';
+import { ProductService } from '../../services/product.service';
+import { AuthService } from 'src/app/services';
+import { LoaderService } from 'src/app/services/loader.service';
+
+
 
 export interface tag {
   name: string;
@@ -21,30 +27,36 @@ export class AddProductComponent implements OnInit {
     description: new FormControl('', Validators.required),
     prix: new FormControl('', Validators.required),
     quantite: new FormControl('', Validators.required),
+    tags: new FormControl('')
   })
 
-  constructor() { }
+  get nom() { return this.productForm.get('nom'); }
+  get description() { return this.productForm.get('description'); }
+  get prix() { return this.productForm.get('prix'); }
+  get quantite() { return this.productForm.get('quantite'); }
 
-  ngOnInit() {
-  }
+
   visible = true;
   selectable = true;
   removable = true;
   addOnBlur = true;
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
-  tags: tag[] = [
-  ];
+  tags: tag[] = [];
+  tagarray: string[];
+  TagChaine : string;
+
+  constructor(private productService: ProductService, private auth: AuthService, private loader: LoaderService) { }
+
+  ngOnInit() {
+  }
 
   add(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
 
-    // Add our fruit
     if ((value || '').trim()) {
       this.tags.push({name: value.trim()});
     }
-
-    // Reset the input value
     if (input) {
       input.value = '';
     }
@@ -52,9 +64,38 @@ export class AddProductComponent implements OnInit {
 
   remove(Tag: tag): void {
     const index = this.tags.indexOf(Tag);
-
     if (index >= 0) {
       this.tags.splice(index, 1);
     }
+  }
+
+
+  onSubmit() {
+    console.log(this.productForm.controls.nom.value);
+    console.log(this.productForm.controls.description.value);
+    console.log(this.productForm.controls.prix.value);
+    console.log(this.productForm.controls.quantite.value);
+    console.log(this.tags)
+    this.TagChaine= "";
+
+    this.tags.forEach(element =>{
+      this.TagChaine += element.name + ";";
+    });
+
+    this.productService.AddProduct(this.productForm.controls.nom.value,
+      this.productForm.controls.prix.value,
+      this.auth.currUser.iduser,
+      this.productForm.controls.quantite.value,
+      uuid(),
+      this.productForm.controls.description.value,
+      this.TagChaine
+    ).subscribe(
+      (res) => {
+        
+      },
+      (err) => {
+
+      }
+    );
   }
 }
