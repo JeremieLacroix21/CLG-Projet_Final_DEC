156a8e2f9745eb1553ad07dd526c0609bd1dbdfa
GabrielF-C <45073321+GabrielF-C@users.noreply.github.com>
2019-10-29 20:32:01 -0400

    .

--- frontend2/expressShop/src/app/services/product.service.ts
+++ frontend2/expressShop/src/app/services/product.service.ts
@@ -87,15 +87,15 @@ export class ProductService {
     );
   }
 
-  GetFournisseurPanier(idproduit)
-  {
+  GetFournisseurPanier(idproduit) {
     const body = new HttpParams()
-      .set('idproduits', idproduit.toString())
-      return this.http.post(
-        `${config.apiUrl}/api/GetFournisseurParCommande`,
-        body.toString(),
-        config.headerObject
-      );
+      .set('idproduits', idproduit.toString());
+
+    return this.http.post(
+      `${config.apiUrl}/api/GetFournisseurParCommande`,
+      body.toString(),
+      config.headerObject
+    );
   }
 
   CreationCommmande(idFournisseur, idDistributeur)
@@ -105,6 +105,7 @@ export class ProductService {
       .set('idDistributeur', idDistributeur.toString())
       .set('complete', "0")
       .set('dateCreation', formatDate(new Date(), 'yyyy/MM/dd', 'en'))
+
       return this.http.post(
         `${config.apiUrl}/api/InsertCommande`,
         body.toString(),
@@ -118,6 +119,7 @@ export class ProductService {
     .set('idCommande', idCommande.toString())
     .set('idProduit', idproduit.toString())
     .set('quantite', quantite.toString())
+
     return this.http.post(
       `${config.apiUrl}/api/InsertCommandeItems`,
       body.toString(),
@@ -125,11 +127,11 @@ export class ProductService {
     );
   }
 
-  EnvoieCommande(idFournisseur, idDistributeur)
-  {
+  EnvoieCommande(idFournisseur, idDistributeur) {
     const body = new HttpParams()
-    .set('idFournisseur', idFournisseur.toString())
-    .set('idDistributeur', idDistributeur.toString())
+      .set('idFournisseur', idFournisseur.toString())
+      .set('idDistributeur', idDistributeur.toString());
+
     return this.http.post(
       `${config.apiUrl}/api/EnvoieCommande`,
       body.toString(),
